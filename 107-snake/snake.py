beaec017118fd7d790e161b978aef4822d6f38cc
akinhet <piti200406@gmail.com>
2020-12-14 02:25:13 +0100

    snake progress - added tail

--- 107-snake/snake.py
+++ 107-snake/snake.py
@@ -24,17 +24,40 @@ class Snake():
         self.width = wid/hor_tiles
         self.shape = pygame.Rect(self.x, self.y, self.width, self.height)
         self.color = (255, 255, 255)
+        self.tail = []
+        for i in range(5):
+            self.tail.append(pygame.Rect(self.x, self.y, self.width, self.height))
 
     def move(self, vx, vy):
+        # Moving the tail
+        tempx1 = self.x
+        tempy1 = self.y
+        tempx2 = 0
+        tempy2 = 0
+        for x in self.tail:
+            tempx2 = x[0]
+            tempy2 = x[1]
+            x[0] = tempx1
+            x[1] = tempy1
+            tempx1 = tempx2
+            tempy1 = tempy2
+
         self.x += vx
         self.y += vy
+        # Checking if the head hit the borders of the screen
         if self.x < 0 or self.x + self.width > wid or self.y < 0 or self.y + self.height > hei:
             self.x = wid/2
             self.y = hei/2
+            for i in self.tail:
+                i[0] = self.x
+                i[1] = self.y
+
         self.shape = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def draw(self):
         pygame.draw.rect(screen, self.color, self.shape, 0)
+        for i in self.tail:
+            pygame.draw.rect(screen, self.color, i, 0)
 
 
 player = Snake(wid/2, hei/2)
